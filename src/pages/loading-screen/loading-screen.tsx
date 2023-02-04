fb3a1d58837da280ffd4c5cde5e7457835a43e54
Dmitry T <92183078+demetr1ss@users.noreply.github.com>
2023-02-04 22:53:38 +0300

    добавит новый спинер + доработает модалку

--- src/pages/loading-screen/loading-screen.tsx
+++ src/pages/loading-screen/loading-screen.tsx
@@ -3,8 +3,9 @@ import styles from './loading-screen.module.css';
 export default function LoadingScreen(): JSX.Element {
   return (
     <div className={styles.container} data-testid='loading-screen'>
-      <div className={styles['loadingio-spinner-eclipse-jkcplxd8gul']} >
-        <div className={styles['ldio-2o7ly9hq75l']}><div></div></div>
+      <div className={styles['loadingio-spinner-ellipsis-ag7tpvlhln6']} >
+        <div className={styles['ldio-yr2ov96pdsr']}><div></div><div></div><div></div><div></div><div></div>
+        </div>
       </div >
     </div>
   );
